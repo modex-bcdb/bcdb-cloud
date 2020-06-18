aacfa57c5da18418406923743a088185913407a4
Gabriel Avasiloae <agabriel777@gmail.com>
2020-06-18 15:19:41 +0300

    add deploy prereq

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Copyright (C) 2019, Ingenium Blockchain Technologies - All Rights Reserved
+#
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+#
+# --------------------------------------------------------------
+# File: ansible.sh
+# File Created: Friday, 29th April 2020 09:35:03
+# Author: Avasiloae Gabriel (gabriel.avasiloae@modex.tech)
+# --------------------------------------------------------------
+#
+# run: sudo bash deploy.sh
+#
+apt install -y docker.io
+curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+exit
+sudo gpasswd -a $USER docker
+newgrp docker
+docker -v && docker-compose -v

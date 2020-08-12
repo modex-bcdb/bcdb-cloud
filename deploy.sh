eb4f92b8bd6c8dff62a8364b41f7f4504b65f475
Gabriel Avasiloae <67112759+gabriel-modex@users.noreply.github.com>
2020-08-12 09:32:36 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,12 +11,12 @@
 # Author: Avasiloae Gabriel (gabriel.avasiloae@modex.tech)
 # --------------------------------------------------------------
 #
-# run: sudo bash deploy.sh
+# run: sudo ./deploy.sh
 #
+export USER=zeus
 apt install -y docker.io
-curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-exit
-sudo gpasswd -a $USER docker
+gpasswd -a $USER docker
 newgrp docker
 docker -v && docker-compose -v
